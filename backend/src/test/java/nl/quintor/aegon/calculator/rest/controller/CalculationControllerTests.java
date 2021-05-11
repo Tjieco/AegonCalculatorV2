09593d15e2b807f9341c321822245a3dff93cabd
Tjieco <tvdonselaar@quintor.nl>
2021-05-11 12:53:16 +0200

    Increased tests for calculations backend

--- backend/src/test/java/nl/quintor/aegon/calculator/rest/controller/CalculationControllerTests.java
+++ backend/src/test/java/nl/quintor/aegon/calculator/rest/controller/CalculationControllerTests.java
@@ -1,5 +1,6 @@
 package nl.quintor.aegon.calculator.rest.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.quintor.aegon.calculator.model.Calculation;
 import nl.quintor.aegon.calculator.model.CalculationType;
@@ -15,10 +16,19 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = CalculationController.class)
 public class CalculationControllerTests {
@@ -27,6 +37,8 @@ public class CalculationControllerTests {
 
     @MockBean
     private CalculationService calculationServiceMock;
+
+    final ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder().build();
 
     @Test
     public void testAdditionCalculation() throws Exception {
@@ -38,7 +50,6 @@ public class CalculationControllerTests {
         actual.setResult(2);
         given(calculationServiceMock.add(1, 1)).willReturn(actual);
 
-        final ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder().build();
         MockHttpServletResponse response = mockMvc.perform(
                 post("/calculate/add")
                         .content(objectMapper.writeValueAsString(payload))
@@ -61,7 +72,6 @@ public class CalculationControllerTests {
         actual.setResult(1);
         given(calculationServiceMock.subtract(2, 1)).willReturn(actual);
 
-        final ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder().build();
         MockHttpServletResponse response = mockMvc.perform(
                 post("/calculate/subtract")
                         .content(objectMapper.writeValueAsString(payload))
@@ -84,7 +94,6 @@ public class CalculationControllerTests {
         actual.setResult(6);
         given(calculationServiceMock.multiply(2, 3)).willReturn(actual);
 
-        final ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder().build();
         MockHttpServletResponse response = mockMvc.perform(
                 post("/calculate/multiply")
                         .content(objectMapper.writeValueAsString(payload))
@@ -107,7 +116,6 @@ public class CalculationControllerTests {
         actual.setResult(3);
         given(calculationServiceMock.divide(6, 2)).willReturn(actual);
 
-        final ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder().build();
         MockHttpServletResponse response = mockMvc.perform(
                 post("/calculate/divide")
                         .content(objectMapper.writeValueAsString(payload))
@@ -126,7 +134,6 @@ public class CalculationControllerTests {
         payload.setFirst(1);
         payload.setSecond(0);
 
-        final ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder().build();
         MockHttpServletResponse response = mockMvc.perform(
                 post("/calculate/divide")
                         .content(objectMapper.writeValueAsString(payload))
@@ -135,5 +142,32 @@ public class CalculationControllerTests {
                 .andReturn().getResponse();
 
         assertThat(response.getStatus(), is(400));
+    }
+
+    @Test
+    public void testGetCalculationsNoHistoryReturns204() throws Exception {
+        mockMvc.perform(get("/calculate/")).andDo(print()).andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void testGetCalculationsAfterRequestReturnsCorrectHistory() throws Exception {
+        Calculation calculation = new Calculation(6, 2, CalculationType.DIVIDE);
+        calculation.setResult(3);
+
+        List<Calculation> history = new ArrayList<>();
+        history.add(calculation);
+        given(calculationServiceMock.getCalculations()).willReturn(history);
+
+        MockHttpServletResponse getResponse = mockMvc.perform(
+                get("/calculate/")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertThat(getResponse.getStatus(), is(200));
+        List<CalculationDto> calculationsResponse = objectMapper.readValue(getResponse.getContentAsString(), new TypeReference<List<CalculationDto>>(){});
+        assertThat(calculationsResponse.get(0).getFirst(), is(6));
+        assertThat(calculationsResponse.get(0).getSecond(), is(2));
+        assertThat(calculationsResponse.get(0).getResult(), is(3.0));
+        assertThat(calculationsResponse.get(0).getType(), is("DIVIDE"));
     }
 }
--- backend/src/test/java/nl/quintor/aegon/calculator/rest/transformer/CalculationTransformerTests.java
+++ backend/src/test/java/nl/quintor/aegon/calculator/rest/transformer/CalculationTransformerTests.java
@@ -1,4 +1,31 @@
 package nl.quintor.aegon.calculator.rest.transformer;
 
+import nl.quintor.aegon.calculator.model.Calculation;
+import nl.quintor.aegon.calculator.model.CalculationType;
+import nl.quintor.aegon.calculator.rest.dto.CalculationDto;
+import nl.quintor.aegon.calculator.service.SimpleCalculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CalculationTransformerTests {
+
+    @Test
+    public void testTransformationCalculationToDTO() {
+        Calculation calculation = new Calculation();
+        calculation.setFirst(100);
+        calculation.setSecond(100);
+        calculation.setType(CalculationType.ADD);
+        calculation.setResult(200);
+        calculation.setId(1234);
+
+        CalculationDto dto = CalculationTransformer.toDto(calculation);
+
+        assertEquals(dto.getFirst(), 100);
+        assertEquals(dto.getSecond(), 100);
+        assertEquals(dto.getType(), "ADD");
+        assertEquals(dto.getResult(), 200);
+    }
 }
